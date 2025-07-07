1bf8f8a9ee795775ed183b8f003cdbd332535eb4
Flaridj <fernanluka2@gmail.com>
2025-07-07 21:23:00 +0200

    Update and rename ai.py to index.html

--- ai.py
+++
@@ -1,6 +0,0 @@
-Fait sans emoji est avec ce modem : # Load model directly
-from transformers import AutoModelForCausalLM
-model = AutoModelForCausalLM.from_pretrained("TheBloke/WizardLM-Uncensored-Falcon-7B-GPTQ", trust_remote_code=True) ou # Use a pipeline as a high-level helper
-from transformers import pipeline
-
-pipe = pipeline("text-generation", model="TheBloke/WizardLM-Uncensored-Falcon-7B-GPTQ", trust_remote_code=True)
